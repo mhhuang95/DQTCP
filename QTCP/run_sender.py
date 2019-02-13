3f7723ceb112f2477ff78c0eb2b21665004143a3
mhhuang95 <mhhuang@ucdavis.edu>
2019-02-13 14:53:38 -0800

    run sender

---
+++ QTCP/run_sender.py
@@ -0,0 +1,36 @@
+
+
+import argparse
+import project_root
+import numpy as np
+import tensorflow as tf
+from os import path
+from env.sender import Sender
+from Q_model import Q_network
+from helpers.helpers import normalize, one_hot, softmax
+
+class Learner(object):
+    def __init__(self, state_dim, action_cnt, restore_vars):
+        self.aug_state_dim = state_dim + action_cnt
+        self.action_cnt = action_cnt
+        self.prev_action = action_cnt - 1
+
+        with tf.variable_scope('global'):
+            self.model = Q_network(state_dim = self.aug_state_dim, action_cnt = self.action_cnt)
+
+        self.
+
+
+        self.sess = tf.Session()
+
+        saver = tf.train.Saver(self.model.trainable_vars)
+        saver.restore(self.sess, restore_vars)
+
+
+    def Q_policy(self, state):
+        norm_state = normalize(state)
+
+        one_hot_action = self.prev_action, self.action_cnt
+
+
+
