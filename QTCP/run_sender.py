af6bc21f20db38bf445326fca4791c3d3c82c6c4
mhhuang95 <mhhuang@ucdavis.edu>
2019-02-14 16:32:26 -0800

    run sender for q learning

--- QTCP/run_sender.py
+++ QTCP/run_sender.py
@@ -18,7 +18,7 @@ class Learner(object):
         with tf.variable_scope('global'):
             self.model = Q_network(state_dim = self.aug_state_dim, action_cnt = self.action_cnt)
 
-        self.
+        self.mainQN = Q_network()
 
 
         self.sess = tf.Session()
@@ -27,10 +27,28 @@ class Learner(object):
         saver.restore(self.sess, restore_vars)
 
 
+    def sample_action(self, state):
+        norm_state = normalize(state)
+
+        one_hot_action = one_hot(self.prev_action, self.action_cnt)
+        aug_state = norm_state + one_hot_action
+
+        # Get probability of each action from the local network.
+        pi = self.mainQN
+        feed_dict = {
+            pi.input: [[aug_state]]
+        }
+        ops_to_run = pi.action_probs
+        action_probs = self.sess.run(ops_to_run, feed_dict)
+
+        # Choose an action to take
+        action = np.argmax(action_probs[0][0])
+        self.prev_action = action
+
+        return action
+
     def Q_policy(self, state):
         norm_state = normalize(state)
 
         one_hot_action = self.prev_action, self.action_cnt
-
-
 
